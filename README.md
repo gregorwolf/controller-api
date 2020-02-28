6390e6771e2655f78f7b2cf2328e621835fb822a
Gregor Wolf <gregor.wolf@gmail.com>
2020-02-28 12:32:11 +0100

    Define destination url in package.json

--- README.md
+++ README.md
@@ -4,20 +4,7 @@ Sample project to consume the XS Controller API from CAP
 
 ## Run local
 
-To run against the local mock service [controller-api-mock](https://github.com/gregorwolf/controller-api-mock) you have to create a file named *default-env.json* with the following content:
-
-```
-{
-  "VCAP_SERVICES": {
-  },
-  "destinations": [
-    {
-      "name": "controller-config",
-      "url": "http://localhost:3000/v2"
-    }
-  ]
-}
-```
+For local testing you have to start the mock service [controller-api-mock](https://github.com/gregorwolf/controller-api-mock) before this app.
 
 Then execute:
 
--- srv/controller-service.js
+++ srv/controller-service.js
@@ -3,13 +3,17 @@ module.exports = cds.service.impl (srv => {
   srv.on('READ', 'XSAUsers', async (req) => {
     const srv = cds.connect.to('controller-config')
     const tx = srv.transaction(req)
-    const response = await tx.get('/users')
-    // try to query the users
-    const users = [
-      {
-        username: "GWOLF"
-      }
-    ]
+    let response
+    try {
+      // try to query the users
+      response = await tx.get('/users')
+    } catch (error) {
+      console.log(error.stack)
+    }
+    let users = []
+    response.users.forEach(function(item){
+      users.push({username: item.userEntity.username})
+    })  
     return users;
   })
 
