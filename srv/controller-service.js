e8ebd414dc51f66b3ca51cf793446a6ac49ce853
Gregor Wolf <gregor.wolf@gmail.com>
2020-03-02 21:05:27 +0100

    Move connect into try / catch

--- srv/controller-service.js
+++ srv/controller-service.js
@@ -1,14 +1,14 @@
 module.exports = cds.service.impl (srv => {
 
   srv.on('READ', 'XSAUsers', async (req) => {
-    const srv = cds.connect.to('controller-config')
-    const tx = srv.transaction(req)
     let response
     try {
+      const srv = cds.connect.to('controller-config')
+      const tx = srv.transaction(req)
       // try to query the users
       response = await tx.get('/users')
     } catch (error) {
-      console.log(error.stack)
+      console.log("error.stack: " + error.stack)
     }
     let users = []
     response.users.forEach(function(item){
