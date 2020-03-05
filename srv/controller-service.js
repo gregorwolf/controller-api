e38d3446c881c8da52953356d71e655e8027ac1d
Gregor Wolf <gregor.wolf@gmail.com>
2020-03-05 18:04:24 +0100

    Adjust Base URL

--- srv/controller-service.js
+++ srv/controller-service.js
@@ -7,12 +7,13 @@ module.exports = cds.service.impl (srv => {
       const srv = cds.connect.to('controller-config')
       const tx = srv.transaction(req)
       // try to query the users
-      response = await tx.get('/users')
+      response = await tx.get('/v2/users')
       response.users.forEach(function(item){
         users.push({username: item.userEntity.username})
       })
     } catch (error) {
       console.log("error.config.baseURL: " + error.config.baseURL)
+      console.log("error.config.url: " + error.config.url)
       console.log("error.stack: " + error.stack)
       req.error(error.response.status, error.response.data)
     }
