7764202edfaad824976bd6d02ce420905f68960c
Gregor Wolf <gregor.wolf@gmail.com>
2020-03-05 17:55:11 +0100

    Better error handling

--- srv/controller-service.js
+++ srv/controller-service.js
@@ -2,18 +2,20 @@ module.exports = cds.service.impl (srv => {
 
   srv.on('READ', 'XSAUsers', async (req) => {
     let response
+    let users = []
     try {
       const srv = cds.connect.to('controller-config')
       const tx = srv.transaction(req)
       // try to query the users
       response = await tx.get('/users')
+      response.users.forEach(function(item){
+        users.push({username: item.userEntity.username})
+      })
     } catch (error) {
+      console.log("error.config.baseURL: " + error.config.baseURL)
       console.log("error.stack: " + error.stack)
+      req.error(error.response.status, error.response.data)
     }
-    let users = []
-    response.users.forEach(function(item){
-      users.push({username: item.userEntity.username})
-    })  
     return users;
   })
 
