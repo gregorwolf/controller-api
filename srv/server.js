554e62aedc5d05b7729b1806d38c2a1de1c8790c
Gregor Wolf <gregor.wolf@gmail.com>
2020-03-14 18:23:51 +0100

    Check that user authentication gets to backend

--- srv/server.js
+++ srv/server.js
@@ -4,12 +4,35 @@ const express = require("express");
 const cds = require("@sap/cds");
 const proxy = require("@sap/cds-odata-v2-adapter-proxy");
 
+// For authentication test
+const passport = require("passport")
+const xsenv = require("@sap/xsenv")
+xsenv.loadEnv();
+const JWTStrategy = require("@sap/xssec").JWTStrategy
+const services = xsenv.getServices({ xsuaa: { tags: "xsuaa" }})
+passport.use(new JWTStrategy(services.xsuaa))
+
 const host = "0.0.0.0";
 const port = process.env.PORT || 4004;
 
 (async () => {
   const app = express();
 
+  // Authentication using JWT
+  await app.use(passport.initialize())
+  await app.use(
+    passport.authenticate('JWT', { session: false })
+  )
+
+  await app.get('/api/userInfo', function (req, res) {
+    res.header("Content-Type", "application/json");
+    res.send(JSON.stringify(req.user))
+  })
+  await app.get('/api/jwt', function (req, res) {
+    const jwt = /^Bearer (.*)$/.exec(req.headers.authorization)[1]
+    res.header("Content-Type", "application/json");
+    res.send(JSON.stringify({ "JWT": jwt }))
+  })
   // serve odata v4
   await cds
     .connect("db") // ensure database is connected!
