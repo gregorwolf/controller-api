7a46657414b133e21ea34a9259033949a53d0a36
Gregor Wolf <gregor.wolf@gmail.com>
2020-03-15 18:07:54 +0100

    Working version using SAP Cloud SDK and axios

--- srv/controller-service.js
+++ srv/controller-service.js
@@ -1,5 +1,6 @@
 const cds = require('@sap/cds')
-const { retrieveJwt } = require('@sap/cloud-sdk-core')
+const axios = require('axios');
+const { executeHttpRequest, HttpMethod, addDestinationToRequestConfig } = require('@sap/cloud-sdk-core')
 
 module.exports = cds.service.impl (srv => {
   const controllerAPI = cds.connect.to('controller-config')
@@ -8,15 +9,49 @@ module.exports = cds.service.impl (srv => {
     let response
     let users = []
     try {
+      var responseUsers = [] 
+      /*
+      // It seems that CAP does not yet support using the JWT for it's requests
+      //
       // Read jwt and find a way to use it with the CAP service call
-      var jwt = retrieveJwt(req._.req)
-      // console.log("JWT: " + jwt)
-      req.attr.token = jwt
+      console.log("JWT: " + req.attr.token)
       const tx = controllerAPI.transaction(req)
       // try to query the users
       response = await tx.get('/v2/users')
       if(response && response.users) {
-        response.users.forEach(function(item){
+        responseUsers = response.users
+      }
+      */
+      /*
+      // Also Cloud SDK Request doesn't send JWT
+      //
+      const destinationNameAndJwt = { destinationName: 'controller-config', jwt: req.attr.token};
+      const httpRequest = {
+          method: HttpMethod.GET,
+          url: "/v2/users"
+      };
+      response = await executeHttpRequest(destinationNameAndJwt, httpRequest)
+      if(response && response.data && response.data.users) {
+        responseUsers = response.data.users
+      }
+      */
+
+     const destinationNameAndJwt = { destinationName: 'controller-config', jwt: req.attr.token};
+     const httpRequest = {
+        method: HttpMethod.GET,
+        url: "/v2/users"
+      };
+
+      var config = await addDestinationToRequestConfig(destinationNameAndJwt, httpRequest)
+      // Providing the destinationNameAndJwt does not have the effect to send the token so we add it manually
+      config.headers = {"Authorization": "Bearer " + req.attr.token}
+      response = await axios(config)
+      if(response && response.data && response.data.users) {
+        responseUsers = response.data.users
+      }
+
+      if(responseUsers) {
+        responseUsers.forEach(function(item){
           if(item.userEntity.username) {
             users.push({username: item.userEntity.username})
           }
